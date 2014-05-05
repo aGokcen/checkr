924372b55fa5821149a55a1aad58790ff03272dc
mlewisno <mlewisno@gmail.com>
2014-05-05 13:03:58 -0400

    Moved images to img folder

--- providers.py
+++ providers.py
@@ -3,7 +3,7 @@ from YahooLogin import login as yahoo_login
 from HotmailLogin import login as hotmail_login
 
 providers = {
-    'gmail': {'displayName': 'GMail', 'icon': '/static/gmail.png', 'login': gmail_login},
-    'yahoo': {'displayName': 'Yahoo!', 'icon': '/static/yahoo.png', 'login': yahoo_login},
-    'hotmail': {'displayName': 'Hotmail', 'icon': '/static/hotmail.png', 'login': hotmail_login}
+    'gmail': {'displayName': 'GMail', 'icon': '/img/gmail.png', 'login': gmail_login},
+    'yahoo': {'displayName': 'Yahoo!', 'icon': '/img/yahoo.png', 'login': yahoo_login},
+    'hotmail': {'displayName': 'Hotmail', 'icon': '/img/hotmail.png', 'login': hotmail_login}
 }
